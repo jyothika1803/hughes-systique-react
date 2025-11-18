2adcca4ea9fbeaa41bbd42cdc1bb76f798c23375
Jyothika Rajesh <jyothikarajesh1809@gmail.com>
2025-11-18 23:31:10 +0530

    Update bassPath.ts

--- src/constants/bassPath.ts
+++ src/constants/bassPath.ts
@@ -1,4 +1,4 @@
 
 
-export const BASE_URL = 'http://localhost:9000'
-export const BASE_ASSETS_URL = 'http://localhost:9000/uploads/'
+export const BASE_URL = 'https://jyothika-hughes-systique-node.onrender.com'
+export const BASE_ASSETS_URL = 'https://jyothika-hughes-systique-node.onrender.com/uploads/'
